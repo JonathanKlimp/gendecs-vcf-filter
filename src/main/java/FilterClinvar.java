9b96c16c233eeb15317941f3529b080f8c649d80
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2022-03-14 11:34:00 +0100

    Renamed method to removeStatus in VcfReader. Method now gets variables for the review status and input file

--- src/main/java/FilterClinvar.java
+++ src/main/java/FilterClinvar.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FilterClinvar {
     public static void main(String[] args) {
+        String filename = "data/clinvar_20220205.vcf";
+        ArrayList<String> zeroStar = new ArrayList<>(
+                List.of("no_assertion_provided",
+                        "no_assertion_criteria_provided")
+        );
+
         VcfReader vcfReader = new VcfReader();
-        vcfReader.readFile();
+
+        if (vcfReader.removeStatus(filename, zeroStar)) {
+            System.out.println("Successfully removed the 0 star rating lines");
+        }
     }
 }
--- src/main/java/VcfReader.java
+++ src/main/java/VcfReader.java
@@ -1,39 +1,28 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class VcfReader {
-    String filename = "data/clinvar_20220205.vcf";
-    File inputFile = new File(filename);
-    File tempFile = new File("data/tempFile.vcf");
-
-    public ArrayList<String> zeroStar = new ArrayList<>(
-            List.of("no_assertion_provided",
-                    "no_assertion_criteria_provided")
-    );
-
-    public void readFile() {
+    public boolean removeStatus(String inputFile, ArrayList<String> reviewStatus) {
+        File tempFile = new File("data/tempFile.vcf");
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-            File fileObject = new File("data/clinvar_20220205.vcf");
-//            File fileObject = new File("data/lp.vcf");
+            File fileObject = new File(inputFile);
             Scanner reader = new Scanner(fileObject);
             while(reader.hasNextLine()) {
                 String data = reader.nextLine();
-                if(stringContainsItemFromList(data, zeroStar)) {
+                if(stringContainsItemFromList(data, reviewStatus)) {
                     continue;
                 }
                 writer.write(data + System.getProperty("line.separator"));
             }
             reader.close();
             writer.close();
-            boolean successful = tempFile.renameTo(inputFile);
+            return tempFile.renameTo(fileObject);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        return false;
     }
     public static boolean stringContainsItemFromList(String inputString, ArrayList<String> items) {
         return items.stream().anyMatch(inputString::contains);
