0bb83d0796341db08b69cea4ae29c385f72fd51d
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2022-03-14 15:32:16 +0100

    VcfReader now has method matchWithClinvar, which builds regex of each line of the entered vcf which is matched with clinvar using getMatchesClinvar

--- src/main/java/FilterClinvar.java
+++ src/main/java/FilterClinvar.java
@@ -2,17 +2,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FilterClinvar {
+
     public static void main(String[] args) {
-        String filename = "data/clinvar_20220205.vcf";
+        String filenameClinvar = "data/clinvar_20220205.vcf";
+        String filenameTestData = "data/lp.vcf";
         ArrayList<String> zeroStar = new ArrayList<>(
                 List.of("no_assertion_provided",
                         "no_assertion_criteria_provided")
         );
 
-        VcfReader vcfReader = new VcfReader();
+        VcfReader vcfReader = new VcfReader(filenameTestData);
 
-        if (vcfReader.removeStatus(filename, zeroStar)) {
-            System.out.println("Successfully removed the 0 star rating lines");
-        }
+//        if (vcfReader.removeStatus(filenameClinvar, zeroStar)) {
+//            System.out.println("Successfully removed the 0 star rating lines");
+//        }
+        vcfReader.matchWithClinvar();
     }
 }
--- src/main/java/VcfReader.java
+++ src/main/java/VcfReader.java
@@ -1,8 +1,17 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class VcfReader {
+    File vcfFile;
+    public VcfReader(String filename) {
+        vcfFile = new File(filename);
+    }
+
     public boolean removeStatus(String inputFile, ArrayList<String> reviewStatus) {
         File tempFile = new File("data/tempFile.vcf");
         try {
@@ -26,5 +35,45 @@ public class VcfReader {
     }
     public static boolean stringContainsItemFromList(String inputString, ArrayList<String> items) {
         return items.stream().anyMatch(inputString::contains);
+    }
+
+    public void matchWithClinvar() {
+        Map<String, Pattern> stringsToFind = new HashMap<>();
+        try {
+            Scanner reader = new Scanner(vcfFile);
+            while(reader.hasNextLine()) {
+                String data = reader.nextLine();
+                if (data.startsWith("#")) {
+                    continue;
+                }
+                String[] splittedData = data.split("\t");
+                String chromosome = splittedData[0];
+                String position = splittedData[1];
+                String ref = splittedData[3];
+                String alt = splittedData[4];
+                Pattern pattern = Pattern.compile(
+                        String.format("%s\\t%s\\t[0-9]+\\t%s\\t%s.+",
+                                chromosome, position, ref, alt));
+                stringsToFind.put(data, pattern);
+
+            }
+            ArrayList<String> foundMatches = getMatchesClinvar(stringsToFind);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static ArrayList<String> getMatchesClinvar(Map<String, Pattern> stringsToFind) throws IOException {
+        File file = new File("data/clinvar_20220205.vcf");
+        Scanner reader = new Scanner(file);
+        ArrayList<String> foundMatches = new ArrayList<>();
+        while(reader.hasNextLine()) {
+            String currentLine = reader.nextLine();
+            for(Pattern stringToFind: stringsToFind.values()) {
+                if (currentLine.matches(String.valueOf(stringToFind))) {
+                    foundMatches.add(currentLine);
+                }
+            }
+        }
+        return foundMatches;
     }
 }
